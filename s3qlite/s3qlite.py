5cdcfe45eee7bf613ed242969804f8389417f22e
Ben Muschol <benmuschol@gmail.com>
2018-02-01 23:28:28 -0500

    Dead simple funcationality (#1)

--- s3qlite/s3qlite.py
+++ s3qlite/s3qlite.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
-import pprint
+import sqlite3
 import os
+import uuid
+import re
 
 import boto3
 from dotenv import load_dotenv, find_dotenv
@@ -18,7 +20,30 @@ client = session.client(
         aws_secret_access_key=os.environ['DIGITALOCEAN_SECRET_ACCESS_KEY'],
         )
 
-response = client.list_buckets()
 
-if __name__ == '__main__':
-    pprint.pprint(response)
+def run_query_bucket(bucket, query, prefix='', filter_func=None):
+    if filter_func is None:
+        def filter_func(filename):
+            return re.compile('^.*\.(db|sqlite|sqlite3)$').match(filename)
+
+    objects = client.list_objects(Bucket=bucket, Prefix=prefix)['Contents']
+    results = {}
+
+    for obj in objects:
+        if not filter_func(obj['Key']):
+            continue
+
+        results[obj['Key']] = run_query_single_object(bucket, obj['Key'], query)
+    return results
+
+def run_query_single_object(bucket, key, query):
+    dest = os.path.join(str(uuid.uuid4()))
+    client.download_file(bucket, key, dest)
+
+    conn = sqlite3.connect(dest)
+    results = []
+    for row in conn.execute(query):
+        results.append(row)
+
+    os.remove(dest)
+    return results
