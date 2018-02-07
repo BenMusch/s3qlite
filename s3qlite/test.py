2db1d1e423e55ebf021c4c3d5912d1b21f76ac29
Ben Muschol <benmuschol@gmail.com>
2018-02-07 11:33:43 -0500

    Add basic error handling

--- s3qlite/test.py
+++ s3qlite/test.py
@@ -4,6 +4,7 @@ import pprint
 from dotenv import find_dotenv, load_dotenv
 
 import s3qlite
+import error_handlers
 
 load_dotenv(find_dotenv())
 
@@ -15,4 +16,5 @@ client = s3qlite.client(
 client.connect('mittab-backups')
 
 if __name__ == '__main__':
-    print(client.execute('select * from tab_debater'))
+    pprint.pprint(client.execute('select ballot_code from tab_judge',
+                                 on_error=error_handlers.raise_error))
